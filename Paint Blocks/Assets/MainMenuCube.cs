7d771ffb32d7c414880bbf99dd1532f06cfe0dde
BrendanWascher <brendanwascher@hotmail.com>
2018-02-19 12:17:26 -0600

    Main Menu
    Added framework for main menu
    Began script for main menu
    Basic level is laid out (nothing else done to it however)
    Experimented with camera angle for level
    Decided I don't need "Die logic"

---
+++ Paint Blocks/Assets/MainMenuCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCube : MonoBehaviour
+{
+
+    public void RotateToAudioPanel(bool isTo)
+    {
+        if (isTo)
+        {
+            this.gameObject.transform.Rotate(0f, 90f, 0f);
+        }
+        else
+            this.gameObject.transform.Rotate(0f, -90f, 0f);
+    }
+}
